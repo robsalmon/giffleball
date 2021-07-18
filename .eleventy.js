9e498490264e99eb5245561afded580efe1728de
Robert Salmon <robdavidsalmon@msn.com>
2021-07-18 19:04:41 +0100

    Add minifier option

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const { minify } = require("terser");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -10,8 +11,26 @@ module.exports = function(eleventyConfig) {
     "css"
   ]);
 
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+code,
+callback
+
+  )
+  {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+
+      console.error("Terser error: ", err);
+    callback(null, code);
+    }
+  }
+  );
+
   eleventyConfig.addPassthroughCopy("cv");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("scripts");
 
   return {
     dir: {
